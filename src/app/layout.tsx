859136a94cadaceefad28606fcae7d311c3ee7a1
vinayaknehwal98-tech <vinayaknehwal98@gmail.com>
2026-08-10 11:20:37 +0530

    Load global image overlay contrast fix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { SitewideCountUp } from "@/components/motion/sitewide-count-up";
 import { SitewideScrollMotion } from "@/components/motion/sitewide-scroll-motion";
 import "./globals.css";
 import "./light-contrast.css";
+import "./media-overlay-contrast.css";
 import "./tpu-contrast.css";
 import "./opening-animation.css";
 import "./proof-backdrop.css";
